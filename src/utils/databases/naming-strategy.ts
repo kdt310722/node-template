ced1a155930bd3a27b72cedfe1e82b14f139cc17
Diep Dang <kdt310722@gmail.com>
2024-04-05 20:35:40 +0700

    fix: entity custom name

--- src/utils/databases/naming-strategy.ts
+++ src/utils/databases/naming-strategy.ts
@@ -1,8 +1,12 @@
-import { SnakeNamingStrategy } from 'typeorm-naming-strategies'
 import plularize from 'pluralize'
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies'
 
 export class NamingStrategy extends SnakeNamingStrategy {
     public override tableName(className: string, customName: string): string {
+        if (customName?.length) {
+            return super.tableName(className, customName)
+        }
+
         return plularize(super.tableName(className, customName))
     }
 }
